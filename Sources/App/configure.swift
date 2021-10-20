4a6e4eb508dd3c029feb123929636c814088a839
shishir sapkota <sis.ccr@gmail.com>
2021-10-20 15:40:01 +0545

    leaf added

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,13 +1,14 @@
 import Fluent
 import FluentPostgresDriver
 import Vapor
-
+import Leaf
 
 // configures your application
 public func configure(_ app: Application) throws {
     //ap uncomment to serve files from /Public folder
      app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     app.http.server.configuration.port = 3000
+    app.views.use(.leaf)
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? 5433,
